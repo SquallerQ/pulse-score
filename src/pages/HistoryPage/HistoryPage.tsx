8531bde70af6abbb14ceb0d24a6671221657d48e
squallerQ <john199502031@gmail.com>
2026-05-23 21:46:48 +0200

    styles: improve historyPage UI

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -1,4 +1,6 @@
 import styles from './HistoryPage.module.css';
+import plLeague from '../../assets/pl-league-logo-league-list--white.svg';
+
 import championLeagueLogo from '../../assets/champ-league-logo.svg';
 
 import { Season } from '../../components/Season/Season';
@@ -29,12 +31,16 @@ export default function HistoryPage() {
     return <div>Loading league...</div>;
   }
 
+  console.log(leagueCode);
+
   return (
     <div className={styles.container}>
       <div className={styles.leaguesContainer}>
         <div className={styles.leaguesContainer_currentLeague} data-league-code={leagueCode}>
           {leagueCode === 'CL' ? (
             <img className={styles.currentLeagueEmblem} src={championLeagueLogo} alt="Champions League" />
+          ) : leagueCode === 'PL' ? (
+            <img className={styles.currentLeagueEmblem} src={plLeague} alt={currentLeague.name} />
           ) : (
             <img className={styles.currentLeagueEmblem} src={currentLeague.emblem} alt={currentLeague.name} />
           )}
